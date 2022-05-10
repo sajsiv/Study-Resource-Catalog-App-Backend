b8485ca4cafd9cb64736b48c5b21dddaa2f525e4
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-05-10 17:22:57 +0100

    created all of the table queries that work

--- study-resource-db.sql
+++ study-resource-db.sql
@@ -1,21 +1,9 @@
-drop table if exists resources
-drop table if exists users 
-
-CREATE TABLE resources (
-  recommendation_reasoning character varying(255) NOT NULL,
-  original_recommendation integer NOT NULL,
-  userid,
-  creation_date timestamp without time zone NOT NULL,
-  stage character varying(255) NOT NULL,
-  content_type character varying(255) NOT NULL,
-  description text NOT NULL,
-  author_name character varying(255) NOT NULL,
-  url character varying(255) NOT NULL,
-  name character varying(255) NOT NULL,
-  resourceid serial NOT NULL,
-  primary key (resourceid),
-  foreign key (userid) references users (userid)
-);
+drop table if exists tostudy;
+drop table if exists tags;
+drop table if exists likes;
+drop table if exists comments;
+drop table if exists resources;
+drop table if exists users ;
 
 CREATE TABLE users (
   is_faculty boolean NULL,
@@ -24,20 +12,42 @@ CREATE TABLE users (
   primary key (userid)
 );
 
+CREATE TABLE resources (
+  resourceid serial NOT NULL primary key,
+  userid integer,
+  recommendation_reasoning text NOT NULL,
+  original_recommendation integer NOT NULL,
+  creation_date timestamp without time zone NOT NULL,
+  stage character varying(255) NOT NULL,
+  content_type character varying(255) NOT NULL,
+  description text NOT NULL,
+  author_name character varying(255) NOT NULL,
+  url character varying(255) NOT NULL,
+  name character varying(255) NOT NULL,
+  foreign key(userid) references users (userid)
+);
+
+
+
 CREATE TABLE tostudy (
     studyid serial primary key,
+  	resourceid integer,
+  	userid integer,
     foreign key (resourceid) references resources (resourceid),
     foreign key (userid) references users (userid)
 );
 
 CREATE TABLE tags (
     tagid serial primary key,
+  	resourceid integer,
     foreign key (resourceid) references resources (resourceid),
     tag varchar(255) NOT NULL
 );
 
 CREATE TABLE likes (
     likeid serial primary key,
+  	userid integer,
+  	resourceid integer, 
     foreign key (userid) references users (userid),
     foreign key (resourceid) references resources (resourceid),
     like_value boolean NOT NULL
@@ -45,7 +55,14 @@ CREATE TABLE likes (
 
 CREATE TABLE comments (
     commentid serial primary key,
+  	userid integer,
+  	resourceid integer,
     foreign key (userid) references users (userid),
     foreign key (resourceid) references resources (resourceid),
     comment_text varchar(255) NOT NULL 
 );
+
+
+
+
+
