68fb036cc9aead25a2693b1d7d492bb0e7c872ea
Christian Sophocleous <c-sophocleous@hotmail.com>
2022-05-11 17:40:32 +0100

    created post route handler for resource form but is not working

--- server.ts
+++ server.ts
@@ -3,6 +3,18 @@ import { config } from "dotenv";
 import express from "express";
 import cors from "cors";
 
+interface FormDataInterface {
+  resourceName: string;
+  authorName: string;
+  URL: string;
+  description: string;
+  tags: string;
+  resourceType: string;
+  buildPhaseWeek: string;
+  recommendation: string;
+  reasonForRecommendation: string;
+}
+
 config(); //Read .env file lines as though they were env vars.
 
 //Call this script with the environment variable LOCAL set if you want to connect to a local db (i.e. without SSL)
@@ -30,6 +42,31 @@ app.get("/", async (req, res) => {
   const dbres = await client.query('select * from categories');
   res.json(dbres.rows);
 });
+
+app.post('/resources', async (req, res) => {
+  try {
+    const resourceData: FormDataInterface = req.body
+    const {resourceName, 
+      authorName,
+      URL,
+      description,
+      resourceType,
+      buildPhaseWeek,
+      recommendation,
+      reasonForRecommendation} = resourceData
+    const postQuery = `insert into resources 
+    (userid, recommendation_reasoning, original_recommendation, stage, content_type, description, author_name, url, name)
+    values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+    await client.query(postQuery, [7, reasonForRecommendation, recommendation, buildPhaseWeek, resourceType,
+    description, authorName, URL, resourceName  ])
+  }catch(error){
+    res.status(404)
+    console.error(error)
+  }
+
+})
+
 
 
 //Start the server on the given port
